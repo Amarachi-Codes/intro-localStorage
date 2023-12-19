dfca8b3cab1402123b14dcfc0e96e9803b38b82c
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-19 21:19:29 +0100

    local storage intro

---
+++ script.js
@@ -0,0 +1,9 @@
+// console.log({window});
+localStorage.setItem("fullname", "Ethel-Doris Igwilo")
+localStorage.setItem("email", "etheldorisamarachi@gmail.com");
+// console.log(localStorage.length);
+console.log(localStorage.getItem("firstname"));
+localStorage.removeItem("firstname")
+console.log(localStorage.length);
+console.log(localStorage.key(0));
+localStorage.clear()
